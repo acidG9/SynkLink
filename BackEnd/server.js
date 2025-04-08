3516c730213541836b3914fce8384c0ff6494ed7
Akshansh Vaishnav <akshanshvaishnavpaypal@gmail.com>
2025-04-08 14:58:32 +0530

    auth and user controller added

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,26 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import dotenv from "dotenv"
 import dbConnect from "./library/dbConnect.lib.js"
+import cookieParser from "cookie-parser"
+import { v2 as cloudinary } from "cloudinary"
 
 const app= express();
 dotenv.config();
 
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 const PORT= process.env.PORT;
 
